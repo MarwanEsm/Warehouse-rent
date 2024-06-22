83638796123fba1141d51987f46433888dfbb06d
marwanEsm <marwan.esmaail85@gmail.com>
2024-06-22 13:20:36 +0200

    modify offer card

--- project/frontend/src/components/elements/OfferCard/OfferCard.tsx
+++ project/frontend/src/components/elements/OfferCard/OfferCard.tsx
@@ -1,10 +1,12 @@
 import warehouse1 from '../../../assets/photos/warehouse1.jpg';
 import warehouse2 from '../../../assets/photos/warehouse2.jpg';
-import styles from "./OfferCard.module.scss"
-import Button from '../button/Button';
+import styles from "./Offers.module.scss"
+import Button from '../../../components/elements/button/Button';
 import { IOffersProps } from '../../../types/components';
+import { useSelector } from 'react-redux';
+import { IState } from '../../../types/reduxState';
 
-const offers = [
+const OFFERS = [
     { id: 1, photo: warehouse1, size: '1000 sqft', location: "Dubai", price: 5000 },
     { id: 2, photo: warehouse1, size: '2000 sqft', location: "Sharjah", price: 8000 },
     { id: 3, photo: warehouse2, size: '2000 sqft', location: "Abu Dhabi", price: 8000 },
@@ -14,6 +16,11 @@ const offers = [
 ];
 
 const OfferCard = (props: IOffersProps) => {
+
+    const selectedCity = useSelector((state: IState) => state?.offer.selectedLocation)
+
+
+    const offers = selectedCity ? OFFERS.filter(offer => offer.location === selectedCity) : OFFERS
 
     return (
         <div className={styles.offers_container} style={{ display: 'grid', gridTemplateColumns: `repeat(${props.repeatCard}, 1fr)` }}>
